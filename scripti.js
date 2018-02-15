ebf73b551e7f6abeae8c1743d3a00e8a9a1a3332
Animale13 <g.minoccari@gmail.com>
2018-02-16 00:30:19 +0100

    Added all the resources for the site, the css, the html, the scripts and the jquery library

---
+++ scripti.js
@@ -0,0 +1,10 @@
+function trans(obj) {
+  document.getElementById("sposta").style.gridColumnStart= "1";
+  document.getElementById("sposta").style.fontSize= "10px";
+}
+
+$(document).ready(function(){
+  $("#sposta").click(function(){
+     $(".intern").animate({left: '250px'});
+   })
+});
